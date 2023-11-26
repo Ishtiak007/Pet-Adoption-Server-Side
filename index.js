695e9251e97655bda6e0099ed0cf4f95ffc7e32a
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-11-27 00:48:52 +0600

    Created /campigns/:id related api for getting specific one pet details

--- index.js
+++ index.js
@@ -88,6 +88,12 @@ async function run() {
         const result =await cursor.toArray();
         res.send(result);
     });
+    app.get('/campaigns/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await campaignsCollection.findOne(query);
+        res.send(result);
+    });
 
 
 
